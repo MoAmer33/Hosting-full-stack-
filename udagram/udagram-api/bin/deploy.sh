51b3d4b3a9a5d7dcd5c26c0144825774fea7092b
unknown <you@example.com>
2022-11-05 11:10:10 +0200

    hello

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -15,4 +15,4 @@ eb setenv AWS_BUCKET=$AWS_BUCKET &&
 eb setenv JWT_SECRET=$JWT_SECRET &&
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
 eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
-eb setenv URL=$URL &&
+eb setenv URL=$URL 
